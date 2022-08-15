b1a774f7daf4238f43ca524a7214b9f7bcb2285e
olamilekan <issaabubakry@gmail.com>
2022-08-15 21:10:47 +0100

    Then I converted the 24hrs format to 12hrs format

--- index.js
+++ index.js
@@ -22,8 +22,13 @@ setInterval(() => {
      let m = new Date().getMinutes();
      let s = new Date().getSeconds();
 
+     //converting the 24hrs to 12hrs setting
+     
+
      //adding zero before every single digit
-     h = (h > 10 ? "0" + h : )
+     h = (h < 10) ? "0" + h : h
+     m = (m < 10) ? "0" + m : m
+     s = (s < 10) ? "0" + s : s
 
     hours.innerHTML = h;
     minutes.innerHTML = m;
