18138ee89d41662031bbfb897bc309932d8c9962
olamilekan <issaabubakry@gmail.com>
2022-08-15 16:37:35 +0100

    laying emphasis on the creation of didital clock

---
+++ index.js
@@ -0,0 +1,15 @@
+let hour = document.querySelector("#hr");
+let minute = document.querySelector("#mn");
+let second = document.querySelector("#sc");
+
+setInterval(() => {
+    let day = new Date();
+    let hh = day.getHours() * 30;
+    let mm = day.getMinutes() * 6;
+    let ss = day.getSeconds() * 6;
+
+    hour.style.transform = `rotateZ(${hh+(mm/12)}deg)`;
+    minute.style.transform = `rotateZ(${mm}deg)`;
+    second.style.transform = `rotateZ(${ss}deg)`;
+});
+
